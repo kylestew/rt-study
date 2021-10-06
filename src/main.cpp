a436749b6abc4e93839967ccb3bac5d9f3d70c69
Kyle Stewart <kylestew@gmail.com>
2021-10-06 13:15:54 -0600

    Basic camera viewport

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "color.h"
+#include "ray.h"
 #include "stb_image_write.h"
 #include "vec3.h"
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
 int main() {
-    const int image_width = 256;
-    const int image_height = 256;
+    // image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    // (not the best name for this variable)
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     char data[image_width * image_height * 3];
 
     for (int j = image_height - 1; j >= 0; --j) {
         std::cout << "\rScanlines remaining: " << j << ' ' << std::endl;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.55);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
 
             int off = (j * image_width + i) * 3;
-            data[off] = static_cast<char>(j);
-            data[off + 1] = static_cast<char>(255.99 * 0);
-            data[off + 2] = static_cast<char>(255.99 * 0);
-
             write_color(&data[off], pixel_color);
         }
     }
