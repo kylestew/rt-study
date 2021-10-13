c4d2719983505436a8ac3f4357dea754547be470
Kyle Stewart <kylestew@gmail.com>
2021-10-12 18:20:00 -0600

    Antialiasing FTW

--- src/color.h
+++ src/color.h
@@ -1,9 +1,20 @@
 #pragma once
-#include "vec3.h"
 #include <iostream>
 
-void write_color(char *data, color pixel_color) {
-    data[0] = static_cast<char>(255.99 * pixel_color.x());
-    data[1] = static_cast<char>(255.99 * pixel_color.y());
-    data[2] = static_cast<char>(255.99 * pixel_color.z());
+#include "vec3.h"
+
+void write_color(char* data, color pixel_color, int samples_per_pixel) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // divide color by number of samples
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
+    data[0] = static_cast<int>(256 * clamp(r, 0.0, 0.999));
+    data[1] = static_cast<int>(256 * clamp(g, 0.0, 0.999));
+    data[2] = static_cast<int>(256 * clamp(b, 0.0, 0.999));
 }
--- src/utility.h
+++ src/utility.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 
@@ -18,6 +20,24 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 
 inline double degrees_to_radians(double degrees) { return degrees * pi / 180.0; }
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+// Random Functions
+
+inline double random_double() {
+    // random real in [0, 1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // random real in [min, max)
+    return min + (max - min) * random_double();
+}
 
 // Common Headers
 
